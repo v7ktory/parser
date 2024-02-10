b64edcd79a48791f6a4e374ed8b49b1585476719
victor <v7ktory@gmail.com>
2024-02-10 17:40:32 +0700

    added gorutines

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"time"
+	"sync"
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
@@ -19,7 +19,6 @@ type Product struct {
 }
 
 func main() {
-
 	file, err := os.Create("products.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -29,60 +28,87 @@ func main() {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	var products []Product
+	var wg sync.WaitGroup
 
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	numPages := 250
+	pageChan := make(chan int, numPages)
+	resultChan := make(chan []Product, numPages)
+
+	// Отправка номеров страниц в канал
+	for i := 1; i <= numPages; i++ {
+		pageChan <- i
+	}
+	close(pageChan)
+
+	// Запуск горутин для обработки каждой страницы
+	for i := 0; i < 10; i++ { // Запускаем 10 горутин для параллельной обработки страниц
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for page := range pageChan {
+				products := getProducts(ctx, page)
+				if len(products) > 0 {
+					resultChan <- products
+				}
+			}
+		}()
+	}
+
+	// Закрытие resultChan после завершения всех горутин
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	// Получение результатов из горутин и добавление в общий список продуктов
+	for products := range resultChan {
+		for _, p := range products {
+			log.Printf("Received product: %s, Price: %s, URL: %s\n", p.Name, p.Price, "https://poizonshop.ru"+p.Url)
+			writer.Write([]string{"https://poizonshop.ru" + p.Url, p.Name, p.Price})
+		}
+	}
+}
+
+// Функция для получения данных о продуктах с одной страницы
+func getProducts(ctx context.Context, page int) []Product {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
+		chromedp.Flag("headless", true),
 		chromedp.WindowSize(1280, 1080),
 		chromedp.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"),
 		chromedp.Flag("blink-settings", "scriptEnabled=false"),
 	)
 
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	ctx, cancel := chromedp.NewExecAllocator(ctx, opts...)
 	defer cancel()
 
 	ctx, cancel = chromedp.NewContext(ctx)
 	defer cancel()
 
-	for i := 1; i < 250; i++ {
-		page := strconv.Itoa(i)
-		var productNodes []*cdp.Node
+	pageStr := strconv.Itoa(page)
+	var productNodes []*cdp.Node
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate("https://poizonshop.ru/products?brands=Nike&page="+pageStr+"&perPage=40&sizeType=EU&sizeValue=42.5&sort=by-relevance"),
+		chromedp.Nodes(".product-card_product_card__5aPyG", &productNodes, chromedp.ByQueryAll),
+	); err != nil {
+		log.Printf("Error getting products from page %s: %s", pageStr, err)
+		return nil
+	}
+
+	var products []Product
+	for _, node := range productNodes {
+		var name, price, url string
+		url, _ = node.Attribute("href")
 		if err := chromedp.Run(ctx,
-			chromedp.Navigate("https://poizonshop.ru/products?brands=Nike&page="+page+"&perPage=40&sizeType=EU&sizeValue=42.5&sort=by-relevance"),
-
-			chromedp.Evaluate(`window.scrollTo(0, document.documentElement.scrollHeight)`, nil),
-			// Slow down the action so we can see what happen.
-			chromedp.Sleep(1*time.Second),
-			chromedp.Nodes(".product-card_product_card__5aPyG", &productNodes, chromedp.ByQueryAll),
+			chromedp.Text(".product-card_name__amzGC", &name, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(".product-card-price_product_card_price__ei89N", &price, chromedp.ByQuery, chromedp.FromNode(node)),
 		); err != nil {
-			panic(err)
+			log.Printf("Error getting product details from page %s: %s", pageStr, err)
+			continue
 		}
-
-		for _, node := range productNodes {
-			var name, price, url string
-			url, _ = (node.Attribute("href"))
-			err := chromedp.Run(ctx,
-				chromedp.Text(".product-card_name__amzGC", &name, chromedp.ByQuery, chromedp.FromNode(node)),
-				chromedp.Text(".product-card-price_product_card_price__ei89N", &price, chromedp.ByQuery, chromedp.FromNode(node)),
-			)
-
-			if err != nil {
-				log.Fatal("Error:", err)
-			}
-
-			product := Product{
-				Name:  name,
-				Price: price,
-				Url:   url,
-			}
-
-			products = append(products, product)
-		}
+		products = append(products, Product{Name: name, Price: price, Url: url})
 	}
-
-	var data [][]string
-	for _, p := range products {
-		data = append(data, []string{"https://poizonshop.ru" + p.Url, p.Name, p.Price})
-	}
-	writer.WriteAll(data)
+	return products
 }
